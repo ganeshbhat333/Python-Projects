ba195b942bc1d50b5b2ea4d14a6abf101dcd0636
Ganesh Bhat <91879332+ganeshbhat333@users.noreply.github.com>
2025-04-27 22:38:05 +0530

    Add files via upload

---
+++ SQL+Python Analysis.sql
@@ -0,0 +1,70 @@
+show databases;
+USE `Dataplay DB`;
+SHOW TABLES FROM `Dataplay DB`;
+select* from orders;
+
+-- 1find top 10 highest revrenue generating products 
+select product_id,sum(sale_price) as sales
+from orders
+group by product_id
+order by sales desc limit 10;
+
+-- find top 5 selling products in each region
+with cte as (
+select region,product_id,sum(sale_price) as sales
+from orders
+group by region,product_id)
+select*from(
+select*,row_number() over (partition by region order by sales desc) as rn
+from cte) a 
+where rn<=5;
+
+-- find month over month growth comparision for 2022 and 2023 sales eg:jan 2022 vs jan 2023
+
+with cte as (
+select year(order_date)as order_year,month(order_date) as order_month,sum(sale_price) as sales
+from orders
+group by order_year,order_month
+)
+select order_month,sum(case when order_year='2022' then sales else 0 end) as sales_2022,
+sum(case when order_year='2023' then sales else 0 end) as sales_2023
+from cte 
+group by order_month
+order by order_month;
+
+-- For each category which month had highest sales
+with cte as ( 
+SELECT category, DATE_FORMAT(order_date, '%Y%m') AS order_year_month,sum(sale_price) as sales
+FROM orders
+group by category,DATE_FORMAT(order_date, '%Y%m')
+)
+select * from (
+select*,
+row_number() over(partition by category order by sales desc) as rn
+from cte
+) a
+where rn = 1;
+
+-- which sub category had highest growth by profit in 2023 compare to 2022
+with cte as (
+select sub_category,year(order_date)as order_year,sum(sale_price) as sales
+from orders
+group by sub_category,year(order_date)
+),
+cte2 as (
+select sub_category,
+sum(case when order_year='2022' then sales else 0 end) as sales_2022,
+sum(case when order_year='2023' then sales else 0 end) as sales_2023
+from cte 
+group by sub_category)
+select *,((sales_2023-sales_2022)*100/sales_2022) as growth
+from cte2
+order by growth desc
+limit 1;
+
+
+
+
+
+
+
